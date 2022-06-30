837d1807e5f3e4548381abcedee969a2acfa3f82
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-30 02:34:34 -0300

    criando ModelView

---
+++ src/Api/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Api.ViewModel;
+using Core.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.DTO;
+using Services.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserServices services;
+
+        public UserController(IUserServices services)
+        {
+            this.services = services;
+        }
+
+
+        [HttpPost]
+        [Route("/api/v1/create")]
+        public async Task<IActionResult> Create([FromBody] CreateUserViewModel userCreate)
+        {
+            try
+            {
+               var create = await services.CreateAsync(userCreate);
+                return Create(create);
+            }
+            catch (DomainExceptions ex)
+            {
+
+                return BadRequest();
+            }
+            catch(Exception)
+            {
+                return StatusCode(500, "Erro");
+            }
+        }
+
+    }
+}
---
+++ src/Api/ViewModel/CreateUserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.ViewModel
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage ="O nome é necessario")]
+        [MaxLength(80,ErrorMessage = "O nome deve ter no mínimo 80 caracters")]
+        [MinLength(3,ErrorMessage = "O nome deve ter no mínimo 3 caracters")]
+        public string Name { get; set; }
+
+
+
+        [Required(ErrorMessage ="O email é obrigatorio")]
+        [MaxLength(30, ErrorMessage = "O email deve ter no mínimo 180 caracters")]
+        [MinLength(10, ErrorMessage = "O Email deve ter no mínimo 10 caracters")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage ="A senha é obrigatoria")]
+        [MaxLength(30, ErrorMessage = "A senha deve ter no mínimo 30 caracters")]
+        [MinLength(6, ErrorMessage = " A senha deve ter no mínimo 6 caracters")]
+        public string Password { get; set; }
+
+    }
+}
