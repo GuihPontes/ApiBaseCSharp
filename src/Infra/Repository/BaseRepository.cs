b471d0e4cb9697b1d43a39e9d36e811b30d5097d
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-29 22:04:31 -0300

    Repositorio Base criado

---
+++ src/Infra/Repository/BaseRepository.cs
@@ -0,0 +1,64 @@
+﻿using Base.Domain.Entities;
+using Infra.Context;
+using Infra.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : Base.Domain.Entities.Base
+    {
+        private readonly SqlContext context;
+
+        public BaseRepository(SqlContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual async Task<T> CreateAsync(T obj)
+        {
+            context.Add(obj);
+            await context.SaveChangesAsync();
+            return obj;
+        }
+
+        public virtual async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>()
+                                        .AsNoTracking()
+                                        .ToListAsync();
+        }
+
+        public virtual async Task<T> GetAsync(long id)
+        {
+            var obj = await context.Set<T>()
+                                           .AsNoTracking()
+                                           .Where(x => x.Id == id)
+                                           .ToListAsync();
+
+            return obj.FirstOrDefault();
+                
+        }
+
+        public virtual async Task Remove(long id)
+        {
+            var obj = await GetAsync(id);
+            if(obj !=null)
+            {
+                context.Remove(obj);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public virtual async Task<T> UpdateAsync(T obj)
+        {
+            context.Entry(obj).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return obj;
+        }
+    }
+}
