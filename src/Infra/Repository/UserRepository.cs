b4c829c3dfdf3b19c2eab0ce4b79d4b8cf2e9ada
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-29 22:41:20 -0300

    Repository terminou

--- src/Infra/Repository/UserRepository.cs
+++ src/Infra/Repository/UserRepository.cs
@@ -1,4 +1,7 @@
-﻿using Infra.Interfaces;
+﻿using Base.Domain.Entities;
+using Infra.Context;
+using Infra.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,65 @@ using System.Threading.Tasks;
 
 namespace Infra.Repository
 {
-    public class UserRepository 
+    public class UserRepository : BaseRepository<User>,IUserRepository
     {
+        private readonly SqlContext _context;
+        public UserRepository(SqlContext context) : base(context)
+        {
+            _context = context;
+        }
+
+       
+        public async Task<User> GetByEmail(string email)
+        {
+            var user = await _context.Users
+                                           .Where
+                                           (
+                                                x =>
+                                                    x.Email.ToLower() == email.ToLower()
+                                            )
+                                           .AsNoTracking()
+                                           .ToListAsync();
+            return user.FirstOrDefault();
+        }
+
+        public async Task<User> GetByName(string name)
+        {
+            var user = await _context.Users
+                                           .Where
+                                           (
+                                                x =>
+                                                    x.Name.ToLower() == name.ToLower()
+                                            )
+                                           .AsNoTracking()
+                                           .ToListAsync();
+            return user.FirstOrDefault();
+        }
+
+        public async Task<ICollection<User>> SerchByEmail(string email)
+        {
+            var userAll = await _context.Users
+                                                .Where
+                                                 (
+                                                        x =>
+                                                            x.Email.ToLower().Contains(email.ToLower())
+                                                  )
+                                                 .AsNoTracking()
+                                                 .ToListAsync();
+            return userAll;
+        }
+
+        public async Task<ICollection<User>> SerchByName(string name)
+        {
+            var userAll = await _context.Users
+                                               .Where
+                                                (
+                                                       x =>
+                                                           x.Name.ToLower().Contains(name.ToLower())
+                                                 )
+                                                .AsNoTracking()
+                                                .ToListAsync();
+            return userAll;
+        }
     }
 }
