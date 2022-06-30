7b13c376a907e14558dc0fdcacf441255cecc121
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-29 21:40:11 -0300

    Criando Interfaces para o repositorio

---
+++ src/Infra/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Base.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Interfaces
+{
+    public interface IBaseRepository<T> where T : Base.Domain.Entities.Base
+    {
+        Task<T> CreateAsync(T obj);
+        Task<T> UpdateAsync(T obj);
+        Task Remove(long id);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetAsync(long id);
+    }
+}
---
+++ src/Infra/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Base.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Interfaces
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<User> GetByEmail(string email);
+        Task<ICollection<User>> SerchByEmail(string email);
+        Task<User> GetByName(string name);
+        Task<ICollection<User>> SerchByName(string name);
+
+    }
+}
