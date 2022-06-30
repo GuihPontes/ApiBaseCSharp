579e40ed5695fae444a48faf8bac60f96b87c6ac
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-29 23:14:27 -0300

    Mapper da classe para as tabelas no banco de dados

--- src/Infra/Mapping/UserMap.cs
+++ src/Infra/Mapping/UserMap.cs
@@ -1,22 +1,43 @@
 ﻿using Base.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infra.Mapping
 {
-    public class UserMap : EntityTypeConfiguration<User>
+    public class UserMap : IEntityTypeConfiguration<User>
     {
-        public void Configure(EntityTypeConfiguration<User> builder)
+        public void Configure(EntityTypeBuilder<User> builder)
         {
             builder.ToTable("User");
 
             builder.HasKey(x => x.Id);
 
+            builder.Property(x => x.Id)
+                .UseIdentityColumn()
+                .HasColumnType("BIGINT");
 
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(80)
+                .HasColumnName("name")
+                .HasColumnType("VARCHAR(80)");
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(180)
+                .HasColumnType("VARCHAR(180)")
+                .HasColumnName("email");
+
+            builder.Property(x => x.Password)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnName("password")
+                .HasColumnType("VARCHAR(30)");
         }
     }
 }
