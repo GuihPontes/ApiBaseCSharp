38fcc1e333b1bd26012378d6d89c54bebcd69ab7
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-30 00:18:44 -0300

    Criacao UserDTO

---
+++ src/Services/Intefaces/IUserServices.cs
@@ -0,0 +1,22 @@
+﻿using Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Intefaces
+{
+    public interface IUserServices
+    {
+        Task<UserDTO> CreateAsync(UserDTO userDTO);
+        Task<UserDTO> UpdateAsync(UserDTO userDTO);
+        Task Remove(long id);
+        Task<List<UserDTO>> GetAllAsync();
+        Task<UserDTO> GetAsync(long id);
+        Task<UserDTO> GetByEmail(string email);
+        Task<ICollection<UserDTO>> SerchByEmail(string email);
+        Task<UserDTO> GetByName(string name);
+        Task<ICollection<UserDTO>> SerchByName(string name);
+    }
+}
